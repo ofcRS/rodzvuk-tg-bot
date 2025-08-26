e105117cd8d8d1d32393f17c194bd2df41e9e570
Aleksandr Sakhatskiy <azeric054@gmail.com>
2025-08-27 00:34:55 +0300

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'rodzvuk-tg-bot',
+      script: './bot.js',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+};
+
+
